a661de75f84b1bb0deab03c0d7bb7e70e21b3fc8
MariaAddict <mashakan27@gmail.com>
2021-05-20 00:44:14 +0700

    feat: создать основные стили страницы, сверстать Header, добавить шрифты и изображения

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+      <header className="header">
+        <h1 className="header__title">Weather<br/><span className="header__title-part">forecast</span></h1>
+      </header>
+  );
+}
+
+export default Header;
