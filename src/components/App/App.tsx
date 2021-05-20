5b849bd4e157dddd34eb856e42bc236a2448a3ce
MariaAddict <mashakan27@gmail.com>
2021-05-20 13:47:32 +0700

    feat: сверстать Footer, адаптировать

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+      <footer className="footer">
+        <p className="footer__discription">C Любовью от Mercury Development</p>
+      </footer>
+  );
+}
+
+export default Footer;
