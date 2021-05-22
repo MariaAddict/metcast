7c96a78c413805ef87f3a9826d505d1e62dee0bb
MariaAddict <mashakan27@gmail.com>
2021-05-22 18:41:59 +0700

    feat: написать функцию считывания данных с inputб добавить проверку на изменение полей

--- src/components/SearchForecast/SearchForecast.tsx
+++ src/components/SearchForecast/SearchForecast.tsx
@@ -1,4 +1,5 @@
 import './SearchForecast.css';
+import React, { useState, useEffect } from 'react';
 import InfoBlock from '../InfoBlock/InfoBlock';
 import CardList from '../CardList/CardList';
 import image from '../../images/Academy-Weather-bg160.svg'
@@ -19,7 +20,7 @@ const cards: CardListProps = [{
     date: '2021-05-28',
     temperature: 27,
     icon: image,
-},{
+}, {
     date: '2021-05-29',
     temperature: 5,
     icon: image,
@@ -37,12 +38,45 @@ interface SearchForecastProps {
     forecast: string,
 }
 
-function SearchForecast(props: SearchForecastProps) {
+function SearchForecast(this: any, props: SearchForecastProps) {
+
+    const [city, setCity] = useState<string>('');
+    const [date, setDate] = useState<string>('');
+    const form = React.createRef<HTMLFormElement>();
+
+    function handleChangeSelect(event: React.ChangeEvent<HTMLSelectElement>): void {
+        setCity(event.target.value);
+        console.log(event.target.value);
+    }
+
+    function handleChangeDate(event: React.ChangeEvent<HTMLInputElement>): void {
+        setDate(event.target.value);
+        console.log(event.target.value);
+    }
+
+    useEffect(():void => {
+        if (city !== '' && date !== '' && form !== null) {
+            // e.preventDefault();
+            // form.current!.submit();
+            //ф-ция вызывающая api
+            console.log(city, date);
+        } else  if (props.forecast === '7days' && city !== '') {
+            // e.preventDefault();
+            // form.current!.submit();
+            //ф-ция вызывающая api
+            console.log(city);
+        }
+    }, [city, date, form, props.forecast]);
+
+    // const onFocus = () => {
+    //     this.type="date";
+    // }
+
     return (
         <div className="forecast">
             <h3 className="forecast__title">{props.forecast === '7days' ? '7 Days Forecast' : 'Forecast for a Date in the Past'}</h3>
-            <form className="forecast__form" name={props.forecast}>
-                <select className="forecast__input" required >
+            <form ref={form} className="forecast__form" name={props.forecast}>
+                <select className="forecast__input" required onChange={handleChangeSelect}>
                     <option hidden disabled selected>Select city</option>
                     <option className="forecast__select-item">Самара</option>
                     <option className="forecast__select-item">Тольятти</option>
@@ -50,7 +84,10 @@ function SearchForecast(props: SearchForecastProps) {
                     <option className="forecast__select-item">Казань</option>
                     <option className="forecast__select-item">Краснодар</option>
                 </select>
-                {props.forecast === 'day' && <input type="date" className="forecast__input" placeholder='Select date' required ></input>}
+                {props.forecast === 'day' && 
+                (
+                    <input type="date" className="forecast__input forecast__input_type_data" placeholder='Select date' required onChange={handleChangeDate}></input>
+                )}
             </form>
             {/* <InfoBlock /> */}
             <CardList cards={cards} forecast={props.forecast} />
