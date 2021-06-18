9fac196ca5be1d9f4969808d42a307bc561f3126
MariaAddict <mashakan27@gmail.com>
2021-06-18 19:09:58 +0700

    Add JSDoc for map in map.js

--- src/utils/map.js
+++ src/utils/map.js
@@ -1,4 +1,18 @@
 // map<T, V>(callback: (element: T) => V, collection: T[]): V[]
+/** 
+ * @template T,V 
+ * */
+/** 
+ * The function returns a new array with a new element returned from callback. Callback - Item update function that takes an element T and return item update
+ * @callback callbackMap
+ * @param {T} element - item
+ * @returns {V} - item update
+ * /
+ /**
+ * @param {callbackMap} callback  - A callback to run.
+ * @param {T[]} array  - Inital array
+ * @returns {V[]} - New array with update array items
+ */
 const map = (callback, array) => {
     return array.reduce((acc, currentVal) => {
         let item = callback(currentVal);
